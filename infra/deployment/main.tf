c9011108667c40d58c814fc4e4446a4e252160db
nasreen96 <nasreensarah@microsoft.com>
2026-03-26 23:17:11 +0530

    updating workflow

--- infra/deployment/main.tf
+++ infra/deployment/main.tf
@@ -1,7 +1,7 @@
 
 
 module "resource_group" {
-  source = "../module/resource_group"
+  source = "../modules/resource_group"
 
   resource_group_name = var.resource_group_name
   location            = var.location
